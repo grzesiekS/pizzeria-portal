cd7b28339f1deb3ebe7d6c888d748a889b7cac65
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-07-30 18:43:35 +0200

    Add subview for Booking component

---
+++ src/components/views/Booking/Booking.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import styles from './Booking.module.scss';
+
+class Booking extends React.Component {
+  render() {
+    return (
+      <div className={styles.component}>
+        <h2>Booking view</h2>
+      </div>
+    );
+  }
+}
+
+export default Booking;
