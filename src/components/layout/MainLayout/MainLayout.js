b00845fec3b1258e5d5bdc03b7bd52ecde4221de
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-07-30 15:07:14 +0200

    Add and implement PageNav component

--- src/components/layout/MainLayout/MainLayout.js
+++ src/components/layout/MainLayout/MainLayout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import PageNav from '../PageNav/PageNav';
 
 class MainLayout extends React.Component {
   render() {
     return (
       <div>
+        <PageNav />
         {this.props.children}
       </div>
     );
