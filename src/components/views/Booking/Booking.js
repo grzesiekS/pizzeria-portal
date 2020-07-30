99201701347fde3798d0700995a99f6304047474
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-07-30 18:55:43 +0200

    Add id passed by Link from TalesStatus to Booking component

--- src/components/views/Booking/Booking.js
+++ src/components/views/Booking/Booking.js
@@ -1,15 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import styles from './Booking.module.scss';
 
 class Booking extends React.Component {
   render() {
+    console.log(this);
     return (
       <div className={styles.component}>
         <h2>Booking view</h2>
+        {/* eslint-disable-next-line no-use-before-define */}
+        <p>{this.props.match.params.id}</p>
       </div>
     );
   }
 }
+
+Booking.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }),
+};
 
 export default Booking;
