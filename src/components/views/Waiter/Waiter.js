0187bd43593eac441437ad84f3fbea5413f4d277
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-07-30 19:10:09 +0200

    Add Links in Waiter component

--- src/components/views/Waiter/Waiter.js
+++ src/components/views/Waiter/Waiter.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 import styles from './Waiter.module.scss';
 
-const Waiter = () => (
+const Waiter = ({id='ID_test'}) => (
   <div className={styles.component}>
     <h2>Waiter view</h2>
+    <Link to={`${process.env.PUBLIC_URL}/waiter/order/new`}>NEW</Link>
+    <Link to={`${process.env.PUBLIC_URL}/waiter/order/${id}`}>ID</Link>
   </div>
 );
+
+Waiter.propTypes = {
+  id: PropTypes.string,
+};
 
 export default Waiter;
