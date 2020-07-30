3520b8ef6e4a52401ae76848837362c0db2c2f08
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-07-30 14:50:51 +0200

    Add Route to dashboard and change sttings inport in style scss

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
+import Dashboard from './components/views/Dashboard/Dashboard';
+
 import MainLayout from './components/layout/MainLayout/MainLayout';
+
 
 function App() {
   return (
-    <MainLayout>
-      <div className='App'>
-        <header className='App-header'>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className='App-link'
-            href='https://reactjs.org'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Learn React Now!
-          </a>
-        </header>
-      </div>
-    </MainLayout>
+    <BrowserRouter basename={'/panel'}>
+      <MainLayout>
+        <Switch>
+          <Route exact path={`${process.env.PUBLIC_URL}/`} component={Dashboard} />
+        </Switch>
+      </MainLayout>
+    </BrowserRouter>
   );
 }
 
