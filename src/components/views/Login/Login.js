33a140e3b5d2a95b38586c9c2641b491e80d63bc
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-08-01 17:53:21 +0200

    Change inputs to TextField from material ui

--- src/components/views/Login/Login.js
+++ src/components/views/Login/Login.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
 
 import styles from './Login.module.scss';
 
 const Login = () => (
   <div className={styles.component}>
     <div className={styles.innerSection}>
-      <h2>Login</h2>
+      <h2 className={styles.title}>Login</h2>
       <form>
-        <input id='login' type='text' placeholder='Login' required />
-        <input id='password' type='password' placeholder='Password' required />
+        <TextField id="login" label="Login" className />
+        <TextField
+          id="password"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+        />
       </form>
     </div>
   </div>
