40053d701dd1a69e57b653f81cc4a748ca881a48
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-08-02 17:02:45 +0200

    Change jsx and constructor code for Booking component

--- src/components/views/Booking/Booking.js
+++ src/components/views/Booking/Booking.js
@@ -12,14 +12,28 @@ import styles from './Booking.module.scss';
 class Booking extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      table1: false,
-      table2: false,
-      table3: false,
-      water: false,
-      bread: false,
-    };
+    this.props.match.params.id === 'new' ?
+      this.state = {
+        table1: false,
+        table2: false,
+        table3: false,
+        water: false,
+        bread: false,
+        defaultDate: '',
+        defaultHour: '12:00',
+        defaultDuration: 0,
+      }
+      :
+      this.state = {
+        table1: true,
+        table2: true,
+        table3: true,
+        water: true,
+        bread: true,
+        defaultDate: '2020-08-02',
+        defaultHour: '15:30',
+        defaultDuration: 3,
+      };
   }
 
   render() {
@@ -31,108 +45,107 @@ class Booking extends React.Component {
     return (
       <div className={styles.component}>
         <h2 className={styles.title}>Booking</h2>
-        {this.props.match.params.id === 'new' ?
-          <form>
-            <TextField
-              id="date"
-              label="Booking Date"
-              type="date"
-              className={styles.inputs}
-              InputLabelProps={{
-                shrink: true,
-              }}
-            />
-            <TextField
-              id="time"
-              label="Booking hour"
-              type="time"
-              defaultValue="12:00"
-              className={styles.inputs}
-              InputLabelProps={{
-                shrink: true,
-              }}
-              inputProps={{
-                min: 12,
-                step: 1800, // 30 min
-              }}
-            />
-            <TextField
-              id="standard-number"
-              label="Duration"
-              type="number"
-              className={styles.inputs}
-              InputLabelProps={{
-                shrink: true,
-              }}
-            />
-            <FormControl component='fieldset' className={styles.inputs}>
-              <FormLabel component="legend">Tables in restaurant</FormLabel>
-              <FormGroup>
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      checked={this.state.table1}
-                      onChange={event => handleChange(event)}
-                      name="table1"
-                      color="primary"
-                    />
-                  }
-                  label="Table-1"
-                />
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      checked={this.state.table2}
-                      onChange={event => handleChange(event)}
-                      name="table2"
-                      color="primary"
-                    />
-                  }
-                  label="Table-2"
-                />
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      checked={this.state.table3}
-                      onChange={event => handleChange(event)}
-                      name="table3"
-                      color="primary"
-                    />
-                  }
-                  label="Table-3"
-                />
-              </FormGroup>
-            </FormControl>
-            <FormControl component='fieldset' className={styles.inputs}>
-              <FormLabel component="legend">Starters</FormLabel>
-              <FormGroup>
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      checked={this.state.water}
-                      onChange={event => handleChange(event)}
-                      name="water"
-                      color="primary"
-                    />
-                  }
-                  label="Water"
-                />
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      checked={this.state.bread}
-                      onChange={event => handleChange(event)}
-                      name="bread"
-                      color="primary"
-                    />
-                  }
-                  label="Bread"
-                />
-              </FormGroup>
-            </FormControl>
-          </form>
-          :
-          <p>{this.props.match.params.id}</p>}
+        <form>
+          <TextField
+            id="date"
+            label="Booking Date"
+            type="date"
+            defaultValue={this.state.defaultDate}
+            className={styles.inputs}
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+          <TextField
+            id="time"
+            label="Booking hour"
+            type="time"
+            defaultValue={this.state.defaultHour}
+            className={styles.inputs}
+            InputLabelProps={{
+              shrink: true,
+            }}
+            inputProps={{
+              min: 12,
+              step: 1800, // 30 min
+            }}
+          />
+          <TextField
+            id="standard-number"
+            label="Duration"
+            type="number"
+            defaultValue={this.state.defaultDuration}
+            className={styles.inputs}
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+          <FormControl component='fieldset' className={styles.inputs}>
+            <FormLabel component="legend">Tables in restaurant</FormLabel>
+            <FormGroup>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={this.state.table1}
+                    onChange={event => handleChange(event)}
+                    name="table1"
+                    color="primary"
+                  />
+                }
+                label="Table-1"
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={this.state.table2}
+                    onChange={event => handleChange(event)}
+                    name="table2"
+                    color="primary"
+                  />
+                }
+                label="Table-2"
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={this.state.table3}
+                    onChange={event => handleChange(event)}
+                    name="table3"
+                    color="primary"
+                  />
+                }
+                label="Table-3"
+              />
+            </FormGroup>
+          </FormControl>
+          <FormControl component='fieldset' className={styles.inputs}>
+            <FormLabel component="legend">Starters</FormLabel>
+            <FormGroup>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={this.state.water}
+                    onChange={event => handleChange(event)}
+                    name="water"
+                    color="primary"
+                  />
+                }
+                label="Water"
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={this.state.bread}
+                    onChange={event => handleChange(event)}
+                    name="bread"
+                    color="primary"
+                  />
+                }
+                label="Bread"
+              />
+            </FormGroup>
+          </FormControl>
+        </form>
       </div>
     );
   }
