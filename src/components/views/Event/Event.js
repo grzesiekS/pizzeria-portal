0964fb8f088ae66c944ee5f1f7dcf3fb575dbdb8
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-08-03 12:08:09 +0200

    Change Event component

--- src/components/views/Event/Event.js
+++ src/components/views/Event/Event.js
@@ -7,40 +7,64 @@ import FormControl from '@material-ui/core/FormControl';
 import FormGroup from '@material-ui/core/FormGroup';
 import FormLabel from '@material-ui/core/FormLabel';
 import Button from '@material-ui/core/Button';
+import shortid from 'shortid';
 
 import styles from './Event.module.scss';
 
+const events = [
+  {
+    id: 1,
+    date: '2020-08-02',
+    hour: '12:30',
+    table: [1,2],
+    repeat: 'daily',
+    duration: 4,
+    ppl: 3,
+    starters: [],
+  },
+  {
+    id: 2,
+    date: '2020-08-02',
+    hour: '17:30',
+    table: [2],
+    repeat: 'daily',
+    duration: 4,
+    ppl: 3,
+    starters: ['bread', 'water'],
+  },
+  {
+    id: 3,
+    date: '2020-08-02',
+    hour: '17:30',
+    table: [3],
+    repeat: 'daily',
+    duration: 4,
+    ppl: 3,
+    starters: ['bread'],
+  },
+];
+
 class Event extends React.Component {
-  constructor(props) {
-    super(props);
-    this.props.match.params.id === 'new' ?
-      this.state = {
-        table1: false,
-        table2: false,
-        table3: false,
-        water: false,
-        bread: false,
-        defaultDate: '',
-        defaultHour: '12:00',
-        defaultDuration: 0,
-        defaultPpl: 0,
-        repeat: false,
-      }
-      :
-      this.state = {
-        table1: true,
-        table2: true,
-        table3: true,
-        water: true,
-        bread: true,
-        defaultDate: '2020-08-02',
-        defaultHour: '15:30',
-        defaultDuration: 3,
-        defaultPpl: 3,
-        repeat: true,
-      };
-  }
+
   render() {
+
+    const filterEvents = eventsData => {
+      const filteredData = [...eventsData].filter(element => element.id.toString() === this.props.match.params.id);
+      if(filteredData.length === 0) {
+        filteredData.push(
+          {
+            id: shortid.generate(),
+            date: '',
+            hour: '',
+            table: [],
+            duration: 0,
+            ppl: 1,
+            starters: [],
+          }
+        );
+      }
+      return filteredData;
+    };
 
     const handleChange = (event) => {
       this.setState({...this.state, [event.target.name]: event.target.checked });
@@ -48,13 +72,13 @@ class Event extends React.Component {
 
     return (
       <div className={styles.component}>
-        <h2 className={styles.title}>Events</h2>
+        <h2 className={styles.title}>{`Event ${this.props.match.params.id}`}</h2>
         <form>
           <TextField
             id="date"
-            label="Booking Date"
+            label="Event Date"
             type="date"
-            defaultValue={this.state.defaultDate}
+            defaultValue={filterEvents(events)[0].date}
             className={styles.inputs}
             InputLabelProps={{
               shrink: true,
@@ -62,9 +86,9 @@ class Event extends React.Component {
           />
           <TextField
             id="time"
-            label="Booking hour"
+            label="Event hour"
             type="time"
-            defaultValue={this.state.defaultHour}
+            defaultValue={filterEvents(events)[0].hour}
             className={styles.inputs}
             InputLabelProps={{
               shrink: true,
@@ -78,7 +102,7 @@ class Event extends React.Component {
             id="standard-number"
             label="Duration"
             type="number"
-            defaultValue={this.state.defaultDuration}
+            defaultValue={filterEvents(events)[0].duration}
             className={styles.inputs}
             InputLabelProps={{
               shrink: true,
@@ -88,7 +112,7 @@ class Event extends React.Component {
             id="standard-number"
             label="People"
             type="number"
-            defaultValue={this.state.defaultPpl}
+            defaultValue={filterEvents(events)[0].ppl}
             className={styles.inputs}
             InputLabelProps={{
               shrink: true,
@@ -100,7 +124,7 @@ class Event extends React.Component {
               <FormControlLabel
                 control={
                   <Checkbox
-                    checked={this.state.table1}
+                    checked={filterEvents(events)[0].table.indexOf(1) !== -1 ? true : false}
                     onChange={event => handleChange(event)}
                     name="table1"
                     color="primary"
@@ -111,7 +135,7 @@ class Event extends React.Component {
               <FormControlLabel
                 control={
                   <Checkbox
-                    checked={this.state.table2}
+                    checked={filterEvents(events)[0].table.indexOf(2) !== -1 ? true : false}
                     onChange={event => handleChange(event)}
                     name="table2"
                     color="primary"
@@ -122,7 +146,7 @@ class Event extends React.Component {
               <FormControlLabel
                 control={
                   <Checkbox
-                    checked={this.state.table3}
+                    checked={filterEvents(events)[0].table.indexOf(3) !== -1 ? true : false}
                     onChange={event => handleChange(event)}
                     name="table3"
                     color="primary"
@@ -138,7 +162,7 @@ class Event extends React.Component {
               <FormControlLabel
                 control={
                   <Checkbox
-                    checked={this.state.water}
+                    checked={filterEvents(events)[0].starters.indexOf('water') !== -1 ? true : false}
                     onChange={event => handleChange(event)}
                     name="water"
                     color="primary"
@@ -149,7 +173,7 @@ class Event extends React.Component {
               <FormControlLabel
                 control={
                   <Checkbox
-                    checked={this.state.bread}
+                    checked={filterEvents(events)[0].starters.indexOf('bread') !== -1 ? true : false}
                     onChange={event => handleChange(event)}
                     name="bread"
                     color="primary"
@@ -165,7 +189,7 @@ class Event extends React.Component {
               <FormControlLabel
                 control={
                   <Checkbox
-                    checked={this.state.repeat}
+                    checked={filterEvents(events)[0].repeat === 'daily' ? true : false}
                     onChange={event => handleChange(event)}
                     name="repeat"
                     color="primary"
