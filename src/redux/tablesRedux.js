13daacabc6cd7e25a4e7092229560bae1a4b0ccb
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-08-18 20:24:03 +0200

    Crate new action type and action creator

--- src/redux/tablesRedux.js
+++ src/redux/tablesRedux.js
@@ -13,11 +13,13 @@ const createActionName = name => `app/${reducerName}/${name}`;
 const FETCH_START = createActionName('FETCH_START');
 const FETCH_SUCCESS = createActionName('FETCH_SUCCESS');
 const FETCH_ERROR = createActionName('FETCH_ERROR');
+const CHANGE_TABLE_STATUS = createActionName('CHANGE_TABLE_STATUS');
 
 /* action creators */
 export const fetchStarted = payload => ({ payload, type: FETCH_START });
 export const fetchSuccess = payload => ({ payload, type: FETCH_SUCCESS });
 export const fetchError = payload => ({ payload, type: FETCH_ERROR });
+export const changeTableStatus = payload => ({ payload, type: CHANGE_TABLE_STATUS });
 
 /* thunk creators */
 export const fetchFromAPI = () => {
