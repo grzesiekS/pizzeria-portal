d9590eeb8b436c3b55a4a7f0607c6c723f1c82fc
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-08-24 21:33:59 +0200

    Modify db structure

--- src/components/views/Order/Order.js
+++ src/components/views/Order/Order.js
@@ -18,53 +18,55 @@ import Button from '@material-ui/core/Button';
 import styles from './Order.module.scss';
 import { Radio } from '@material-ui/core';
 
-const dbDataOrdersDemo = {
-  address: '',
-  phone: '',
-  tableNo: '1',
-  totalPrice: 40,
-  totalNumber: 1,
-  subtotalPrice: 20,
-  deliveryFee: 20,
-  products: [
-    {
-      id: 'pizza',
-      amount: 1,
-      price: 20,
-      priceSingle: 20,
-      params: {
-        sauce: {
-          tomato: 'tomato',
-        },
-        toppings: {
-          olives: 'olives',
-          redPeppers: 'redPeppers',
-          greenPeppers: 'greenPeppers',
-          mushrooms: 'mushrooms',
-          basil: 'basil',
-        },
-        crust: {
-          standard: 'standard',
+const dbDataOrdersDemo = [
+  {
+    address: '',
+    phone: '',
+    tableNo: '1',
+    totalPrice: 40,
+    totalNumber: 1,
+    subtotalPrice: 20,
+    deliveryFee: 20,
+    products: [
+      {
+        id: 'pizza',
+        amount: 1,
+        price: 20,
+        priceSingle: 20,
+        params: {
+          sauce: {
+            tomato: 'tomato',
+          },
+          toppings: {
+            olives: 'olives',
+            redPeppers: 'redPeppers',
+            greenPeppers: 'greenPeppers',
+            mushrooms: 'mushrooms',
+            basil: 'basil',
+          },
+          crust: {
+            standard: 'standard',
+          },
         },
       },
-    },
-    {
-      id: 'cake',
-      amount: 9,
-      price: 27,
-      priceSingle: 9,
-      params: {},
-    },
-    {
-      id: 'cake',
-      amount: 5,
-      price: 45,
-      priceSingle: 9,
-      params: {},
-    },
-  ],
-  id: 123,
-};
+      {
+        id: 'cake',
+        amount: 9,
+        price: 27,
+        priceSingle: 9,
+        params: {},
+      },
+      {
+        id: 'cake',
+        amount: 5,
+        price: 45,
+        priceSingle: 9,
+        params: {},
+      },
+    ],
+    id: 123,
+  },
+];
 
 const findProduct = (productId, productList) => productList.filter(product => product.id === productId);
 
@@ -185,45 +187,47 @@ class Order extends React.Component {
               ))}
             </div>
             <div className={styles.container}>
-              <Grid item component={Paper} className={styles.innerSection}>
-                <h2 className={styles.title}>{`Order ${this.props.match.params.id}`}</h2>
-                <TextField
-                  id="standard-basic"
-                  label="Table No"
-                  className={styles.input}
-                  value={dbDataOrdersDemo.tableNo}
-                />
-                <h2 className={styles.subtitle}>Order details:</h2>
-                {dbDataOrdersDemo.products.map(product => (
-                  findProduct(product.id, products).map(productFilter => (
-                    <div key={product.id} className={styles.orderDetails}>
-                      <p>{`${productFilter.name} x ${product.amount} Price: ${product.price}$`}</p>
-                      {product.params !== undefined
-                        ?
-                        Object.keys(product.params).map(param => (
-                          <ul key={param}>
-                            <li>
-                              {`${productFilter.params[param].label}:`}
-                              <ul>
-                                {Object.keys(product.params[param]).map(paramInner => (
-                                  <li key={paramInner}>{productFilter.params[param].options[paramInner].label}</li>
-                                ))}
-                              </ul>
-                            </li>
-                          </ul>
-                        ))
-                        :
-                        null
-                      }
-                    </div>
-                  ))
-                ))}
-                <h2 className={styles.subtitle}>Total Price:</h2>
-                <h2 className={styles.title}>{`${dbDataOrdersDemo.totalPrice} $`}</h2>
-                <Button variant="contained" color="primary">
-                  Send Order
-                </Button>
-              </Grid>
+              {dbDataOrdersDemo.map(order =>
+                <Grid key={order.id} item component={Paper} className={styles.innerSection}>
+                  <h2 className={styles.title}>{`Order ${this.props.match.params.id}`}</h2>
+                  <TextField
+                    id="standard-basic"
+                    label="Table No"
+                    className={styles.input}
+                    value={order.tableNo}
+                  />
+                  <h2 className={styles.subtitle}>Order details:</h2>
+                  {order.products.map(product => (
+                    findProduct(product.id, products).map(productFilter => (
+                      <div key={product.id} className={styles.orderDetails}>
+                        <p>{`${productFilter.name} x ${product.amount} Price: ${product.price}$`}</p>
+                        {product.params !== undefined
+                          ?
+                          Object.keys(product.params).map(param => (
+                            <ul key={param}>
+                              <li>
+                                {`${productFilter.params[param].label}:`}
+                                <ul>
+                                  {Object.keys(product.params[param]).map(paramInner => (
+                                    <li key={paramInner}>{productFilter.params[param].options[paramInner].label}</li>
+                                  ))}
+                                </ul>
+                              </li>
+                            </ul>
+                          ))
+                          :
+                          null
+                        }
+                      </div>
+                    ))
+                  ))}
+                  <h2 className={styles.subtitle}>Total Price:</h2>
+                  <h2 className={styles.title}>{`${order.totalPrice} $`}</h2>
+                  <Button variant="contained" color="primary">
+                    Send Order
+                  </Button>
+                </Grid>
+              )}
             </div>
           </Grid>
         </div>
