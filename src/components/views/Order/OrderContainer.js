d2de1a9539ae9d22d72522cbc1930bd259f792e3
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-08-24 21:21:08 +0200

    Dispatch to props fetchFilteredOrderfromAPI function

--- src/components/views/Order/OrderContainer.js
+++ src/components/views/Order/OrderContainer.js
@@ -1,14 +1,17 @@
 import { connect } from 'react-redux';
 import Order from './Order';
 import {getAllProducts, fetchProductsFromAPI, getProductsLoadingState} from '../../../redux/productRedux';
+import {getAllOrders, fetchFilteredOrderFromAPI} from '../../../redux/orderRedux';
 
 const mapStateToProps = (state) => ({
   products: getAllProducts(state),
   loadingProducts: getProductsLoadingState(state),
+  orders: getAllOrders(state),
 });
 
 const mapDispatchToProps = (dispatch) => ({
   fetchProducts: () => dispatch(fetchProductsFromAPI()),
+  fetchFilteredOrder: id => dispatch(fetchFilteredOrderFromAPI(id)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Order);
