24ee46a5a9566929715c30915a43b660b6743422
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-08-24 18:13:15 +0200

    Create productRedux with thunk creators

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,6 +3,7 @@ import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 import tablesReducer from './tablesRedux';
+import productReducer from './productRedux';
 
 // define initial state and shallow-merge initial data
 const initialState = {
@@ -18,6 +19,7 @@ const initialState = {
 // define reducers
 const reducers = {
   tables: tablesReducer,
+  product: productReducer,
 };
 
 // add blank reducers for initial state properties without reducers
